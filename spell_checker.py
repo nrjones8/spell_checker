71b1f3cab8700a0939b0fd47a9fe31d22af1e62d
Nick Jones <nrjones8@gmail.com>
2013-10-01 13:42:05 -0500

    Improved UI

--- spell_checker.py
+++ spell_checker.py
@@ -1,6 +1,7 @@
 # Nick Jones
 
 import re
+from heapq import heappush, heappushpop
 
 class SpellChecker:
 
@@ -8,6 +9,7 @@ class SpellChecker:
 		# Read in dictionary, strip newlines from each word
 		self.dictionary = self.get_dictionary(dict_location)
 		self.letter_and_length_dictionary = self.preprocess_dictionary(self.dictionary)
+		self.io = CheckerIO()
 
 	def get_dictionary(self, dict_location):
 		return set([w.strip() for w in file(dict_location).readlines()])
@@ -22,27 +24,32 @@ class SpellChecker:
 
 		return first_letter_to_length_words
 
-	def correct_spelling(self, word):
+	def get_closest_words(self, word, k = 5):
 		'''
-		Returns <word> if <word> is in self.dictionary, or the closest
-		correct word measured by the <edit_distance> function
+		Returns <word> if <word> is in self.dictionary, or the closest <k>
+		words measured by the <edit_distance> function
 		'''
 		if word in self.dictionary:
 			return word
 		else:
-
-			candidate_words = self.initial_filter(word)
-			min_dist, min_word = None, None
-			for cand in candidate_words:
+			# TODO check if there aren't <k> candidates
+			all_candidate_words = self.initial_filter(word)
+			
+			# Each entry (word, distance)
+			candidate_dists = []
+			# Could use a heap here instead
+			for cand in all_candidate_words:
 				cand_dist = edit_distance(word, cand)
-
-				if min_dist == None or cand_dist < min_dist:
-					min_dist = cand_dist
-					min_word = cand
-
-			print 'off by %i' % min_dist
-
-			return min_word
+				if len(candidate_dists) < k:
+					heappush(candidate_dists, (- cand_dist, cand))
+				else:
+					# "peek" at largest distance element, discard it if larger than 
+					# cand_dist
+					if candidate_dists[0] > cand_dist:
+						heappushpop(candidate_dists, (- cand_dist, cand))
+			
+			# Pick off first elements (the actual words)	
+			return [x[1] for x in reversed(candidate_dists)]
 
 	def initial_filter(self, word, delta = 3):
 		'''
@@ -55,22 +62,83 @@ class SpellChecker:
 		max_length_to_check = word_length + delta
 		candidates = set()
 
-		for acceptable_length in range(min_length_to_check, max_length_to_check):
+		for acceptable_length in range(min_length_to_check, max_length_to_check + 1):
 			# Check if there exists a word of length <acceptable_length> that starts with <first_letter>
 			if acceptable_length in self.letter_and_length_dictionary[first_letter]:
 				candidates.update(self.letter_and_length_dictionary[first_letter][acceptable_length])
 
 		return candidates 
 
-	def check(self, document):
-		for word in document:
-			replacement = self.correct_spelling(word)
-			if replacement != word:
-				print 'Replaced %s with %s' % (word, replacement)
+	def check_line(self, line):
+		tokens = tokenize(line)
+		corrected_tokens = tokens
+		corrected_line = line
 
+		for i in range(len(tokens)):
+			word = tokens[i]
+
+			if len(word) != 0:
+				candidate_replacements = self.get_closest_words(word)
+				if candidate_replacements != word:
+					# If misspelled, user selects one of the closest (by edit_distance) replacements
+					corrected_word = self.io.get_replacement_from_user(line, word, candidate_replacements)
+					# Replacement first occurence of <word> in <line> with <corrected_word>
+					corrected_line = corrected_line.replace(word, corrected_word, 1)
+					corrected_tokens[i] = corrected_word
+					
+		return corrected_line
+
+	def write_new_file(self, edited_lines, infile_name):
+		self.io.write_new_file(edited_lines, infile_name)
+		
+
+class CheckerIO:
+
+	def get_replacement_from_user(self, line, word, cands):
+		'''
+		Displays <line> containing misspelled <word> and all <cands> as options 
+		for replacements
+		returns user's selection of replacement
+		'''
+		self.display_options(line, word, cands)
+		replacement_index = raw_input('Enter your selection (number) from the above options:\n')
+
+		try:
+			index = int(replacement_index)
+			replacement = cands[index]
+
+		except ValueError:
+			print 'Couldn\'t parse your selection. Default (option 0) was used'
+			replacement = cands[0]
+
+		except IndexError:
+			print '%s was not an option. Default (option 0) was used' % replacement_index
+			replacement = cands[0]
+
+		print '-' * 20
+		return replacement
+
+	def display_options(self, line, word, cands):
+		start_index = line.index(word)
+
+		print '"%s" was misspelled on the following line:' % word
+		print line.strip()
+		print ' ' * start_index + '^'
+		for i in range(len(cands)):
+			print '%i: %s' % (i, cands[i])
+
+	def write_new_file(self, edited_lines, infile_name):
+		outfile_name = 'corrected_' + infile_name
+		f = open(outfile_name, 'w')
+		for line in edited_lines:
+			f.write(line)
+
+		f.close() 
+		print 'Wrote %s' % outfile_name
 
 def tokenize(text):
 	return re.split(r'\s+', text)
+
 
 def edit_distance(s1, s2, insertion_cost = 1, deletion_cost = 1, sub_cost = 2):
 	'''Calculate edit distance of transforming s1 to s2'''
@@ -112,11 +180,19 @@ def edit_distance(s1, s2, insertion_cost = 1, deletion_cost = 1, sub_cost = 2):
 	return distances[nrow - 1][ncol - 1]
 
 def main():
-	text = file('test_text.txt').read()
-	tokens = tokenize(text)
-	print tokens
 	sc = SpellChecker()
-	sc.check(tokens)
+	infile = 'test_text.txt'
+	
+	all_lines = file(infile).readlines()
+	revised_lines = []
+	for line in all_lines:
+		correct_line = sc.check_line(line)
+		revised_lines.append(correct_line)
+		# print 'Had tokens', tokenize(line)
+		# print 'Now has', correct_line
+	sc.write_new_file(revised_lines, infile)
+	print ''.join(revised_lines)
+	
 
 if __name__ == '__main__':
 	main()
